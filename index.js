bb14534fae1f7d31de0f298bb3f058aae35d7c61
nbross <nbross2001@gmail.com>
2022-08-20 14:26:24 -0400

    added local storage and a delete feature for users

--- index.js
+++ index.js
@@ -2,16 +2,30 @@ let myLeads = []
 const inputEl = document.getElementById('input-el');
 const inputBtn = document.getElementById('input-btn')
 const ulEl = document.getElementById("ul-el");
+const deleteBtn = document.getElementById("delete-btn")
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+
+if(leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    renderLeads();
+} 
+
+deleteBtn.addEventListener("dblclick", function(){
+    localStorage.clear()
+    myLeads = []
+    renderLeads()
+})
 
 inputBtn.addEventListener("click", function () {
     myLeads.push(inputEl.value)
-    console.log(myLeads)
+    inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
     renderLeads()
+    console.log( localStorage.getItem("myLeads") )
 });
 
 function renderLeads() {
-    let listItems = "";
-
+    let listItems = ""
     for (let i = 0; i < myLeads.length; i++) {
         listItems += 
         `<li>
@@ -22,5 +36,4 @@ function renderLeads() {
     }
 
     ulEl.innerHTML = listItems
-    inputEl.value = ""
 }
